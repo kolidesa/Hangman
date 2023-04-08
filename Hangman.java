65e1ac911dc11a928a5424b6f4f5f9c7fc11facf
kolidesa <77902590+kolidesa@users.noreply.github.com>
2023-04-08 11:16:14 -0400

    Added some GUI

--- Hangman.java
+++ Hangman.java
@@ -1,14 +1,28 @@
 import java.util.*;
 import java.io.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class Hangman {
 	
 	static int gallowsNum;
+	static JFrame frame;
+	static String word;
+	static char[] hangmanWord;
+	static boolean complete;
+	static ArrayList<Character> guessedLetters;
+	static JTextField f;
 
 	public static void main(String[] args) throws Exception {
 		Scanner scan = new Scanner(System.in);
 
-		System.out.println("Welcome to Hangman!");
+		frame = new JFrame("Hangman!");
+
+		JPanel panel = new JPanel();
+
+		panel.setLayout(new FlowLayout());
+
 
 		Scanner s = new Scanner(new File("words.txt"));
 
@@ -16,55 +30,99 @@ public class Hangman {
 		Random rand = new Random();
 		int num = rand.nextInt(417147);
 
-		String word = "";
-
 		for (int i = 0; i < num; i++) {
 			word = s.nextLine();
 		}
 
-		char[] hangmanWord = new char[word.length()];
+		hangmanWord = new char[word.length()];
 		for (int i = 0; i < hangmanWord.length; i++) {
 			hangmanWord[i] = '_';
 		}
 
-		boolean complete = false;
+		complete = false;
 		gallowsNum = 0;
-		ArrayList<Character> guessedLetters = new ArrayList<Character>();
-		do {
+		guessedLetters = new ArrayList<Character>();
 
-			printGallows();
+
+
+
+
+		JLabel label;
+		label = new JLabel(getWord(hangmanWord));
+
+		JPanel gallows = printGallows();
+		//gallows.setPreferredSize(new Dimension(5, 100));
+
+		JLabel label2 = new JLabel("Guess a letter: ");
+
+		f = new JTextField(10);
+
+		JButton button = new JButton();
+		button.setText("Submit");
+		button.addActionListener(new ActionListener(){  
+		    public void actionPerformed(ActionEvent e){  
+		            char guess=f.getText().toString().charAt(0);
+			    	boolean contained = false;
+
+					for (int i = 0; i < word.length(); i++) {
+						if (word.charAt(i) == guess) {
+							hangmanWord[i] = guess;
+							contained = true;
+						}
+					}
+					if (!contained) {
+						gallowsNum++;
+						if (guessedLetters.contains(guess) == false)
+							guessedLetters.add(guess);
+					}  
+		    }  
+		});
+
+
+		panel.add(label);
+		panel.add(label2);
+		panel.add(f);
+		panel.add(button);
+
+		frame.add(gallows);
+		frame.add(panel);
+		frame.setSize(600, 400);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+
+		//do {
+
+			//gallows = new JPanel(printGallows());
+
+			//Add gallows
+			
+			
+			/*panel.add(label);
+			panel.add(label2);
+			panel.add(f);
+			panel.add(button);*/
+			//frame.add(gallows);
+			//frame.add(panel);
+			//frame.revalidate();
+			//frame.setVisible(false);
+			//frame.setVisible(true);
 
 			if (gallowsNum == 6) {
 				System.out.println("You lost! The word was " + word);
-				break;
+				//break;
 			}
 
-			printGuessedLetters(guessedLetters);
+			//printGuessedLetters(guessedLetters);
 
-			printWord(hangmanWord);
+			//printWord(hangmanWord);
 
-			System.out.print("Guess a letter: ");
-			char guess = scan.next().charAt(0);
+			/*System.out.print("Guess a letter: ");
+			char guess = scan.next().charAt(0);*/
 
-			boolean contained = false;
+			
 
-			for (int i = 0; i < word.length(); i++) {
-				if (word.charAt(i) == guess) {
-					hangmanWord[i] = guess;
-					contained = true;
-				}
-			}
-
-			if (!contained) {
-				gallowsNum++;
-				if (guessedLetters.contains(guess) == false)
-					guessedLetters.add(guess);
-			}
-			System.out.println();
-
-			complete = checkWord(hangmanWord, word);
-
-		} while (!complete);
+		//} while (!complete);
 
 		if (complete) {
 			printWord(hangmanWord);
@@ -73,30 +131,57 @@ public class Hangman {
 
 	}
 
-	static void printGallows() {
-		System.out.println("  __");
-		System.out.println(" |  |");
+	static String getWord(char[] word) {
+		String s = "";
+		for (int i = 0; i < word.length; i++) {
+			s += word[i] + " ";
+		}
+		return s;
+	}
+
+	static JPanel printGallows() {
+		JPanel t = new JPanel();
+		t.setLayout(new BoxLayout(t, BoxLayout.Y_AXIS));
+
+		JLabel g1 = new JLabel("    __\n");
+		JLabel g2 = new JLabel("   |  |\n");
+
+		JLabel g3;
 		if (gallowsNum > 0)
-			System.out.println(" o  |");
-		else System.out.println("    |");
-
+			g3 = new JLabel(" o  |");
+		else g3 = new JLabel("      |");
 		
+
+		JLabel g4;
 		if (gallowsNum > 3)
-			System.out.println("-|- |");
+			g4 = new JLabel("  -|- |");
 		else if (gallowsNum > 2)
-			System.out.println("-|  |");
+			g4 = new JLabel("  -|  |");
 		else if (gallowsNum > 1)
-			System.out.println(" |  |");
-		else System.out.println("    |");
-
+			g4 = new JLabel("   |  |");
+		else g4 = new JLabel("      |");
+		
+		JLabel g5;
 		if (gallowsNum > 5)
-			System.out.println("/ \\ |");
+			g5 = new JLabel("  / \\ |");
 		else if (gallowsNum > 4)
-			System.out.println("/   |"); 
-		else System.out.println("    |");
+			g5 = new JLabel("  /   |"); 
+		else g5 = new JLabel("      |");
 
-		System.out.println("    |");
-		System.out.println("-----");
+
+		JLabel g6 = new JLabel("      |");
+		JLabel g7 = new JLabel("-----");
+
+
+		t.add(g1);
+		t.add(g2);
+		t.add(g3);
+		t.add(g4);
+		t.add(g5);
+		t.add(g6);
+		t.add(g7);
+
+		return t;
 	}
 
 	static void printWord(char[] word) {
@@ -128,5 +213,7 @@ public class Hangman {
 		System.out.println("---------------");
 	}
 
-
+	/*public void actionPerformed(ActionEvent e) {
+		
+    }*/
 }
